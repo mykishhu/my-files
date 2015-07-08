3b4b534bcf46d4bd7d6ca71b5ab62483ea2b05fd
epattki <pattamatta.ch.n.kishore@ericsson.com>
2015-07-08 12:50:21 +0530

    Added new files

---
+++ maven-stuff/JavaSamples/src/main/java/com/java/samples/Mykishhu.java
@@ -0,0 +1,11 @@
+ublic class Mykishhu{
+
+   public Mykishhu(String name){
+      // This constructor has one parameter, name.
+      System.out.println("Passed Name is :" + name ); 
+   }
+   public static void main(String []args){
+      // Following statement would create an object myMykishhu
+      Mykishhu myMykishhu = new Mykishhu( "tommy" );
+   }
+}
